dc698c1287c8278f0e1f3c700215c1c1daab7fab
Schwifter <d.goglidze@sangu.edu.ge>
2025-11-13 12:49:32 +0400

    Add sidebar components

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,47 @@
+import { Home, Inbox } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarHeader,
+  SidebarFooter,
+} from "@/components/ui/sidebar";
+
+const items = [
+  {
+    title: "First",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Second",
+    url: "#",
+    icon: Inbox,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader>Side-Header</SidebarHeader>
+      <SidebarContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton asChild>
+                <a href={item.url}>
+                  <item.icon />
+                  <span>{item.title}</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarContent>
+      <SidebarFooter>Side-Footer</SidebarFooter>
+    </Sidebar>
+  );
+}
