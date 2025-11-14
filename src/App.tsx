4cf4cf615d1f73d52bc7d06e06d4351ee3aaaa45
Schwifter <d.goglidze@sangu.edu.ge>
2025-11-14 11:41:09 +0400

    Add page title in header

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "@/components/mode-toggle.tsx";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import HomePage from "@/pages/home/page";
 import LearningPage from "@/pages/learning/page";
 import GamingPage from "@/pages/gaming/page";
 import MemesPage from "@/pages/memes/page";
+import { SiteHeader } from "@/components/site-header.tsx";
 
 function App() {
   return (
@@ -26,10 +22,7 @@ function App() {
         >
           <AppSidebar />
           <SidebarInset>
-            <header className="flex justify-between h-16 shrink-0 items-center gap-2 px-4">
-              <SidebarTrigger className="-ml-1" />
-              <ModeToggle />
-            </header>
+            <SiteHeader />
             <Routes>
               <Route path="/" element={<Navigate to="/home" replace />} />
               <Route path="/home" element={<HomePage />} />
