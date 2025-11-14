71c114dfeb36d7d28835378426ab1957b032a2ed
Schwifter <d.goglidze@sangu.edu.ge>
2025-11-14 13:12:33 +0400

    Add space for about me

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,15 +1,16 @@
 export default function HomePage() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+      <div className="bg-muted/50 min-h-[91vh] flex-1 rounded-xl p-6">
+        <h1 className="text-2xl font-bold mb-4">Profile pic</h1>
+        <p>who am I</p>
+      </div>
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
         <div className="bg-muted/50 aspect-video rounded-xl" />
         <div className="bg-muted/50 aspect-video rounded-xl" />
         <div className="bg-muted/50 aspect-video rounded-xl" />
       </div>
-      <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min p-6">
-        <h1 className="text-2xl font-bold mb-4">Home</h1>
-        <p>Welcome to the home page!</p>
-      </div>
+      <div className="bg-muted/50 flex-1 rounded-xl p-6"></div>
     </div>
   );
 }
