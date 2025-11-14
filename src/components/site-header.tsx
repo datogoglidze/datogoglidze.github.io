8eb0e2a6ad463a2947f61a93b7f82564fc0b6b80
David Goglidze <70262653+datogoglidze@users.noreply.github.com>
2025-11-14 11:43:00 +0400

    Add page title in header (#25)

---
+++ src/components/site-header.tsx
@@ -0,0 +1,30 @@
+import { useLocation } from "react-router-dom";
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { ModeToggle } from "@/components/mode-toggle.tsx";
+
+const routeTitles: Record<string, string> = {
+  "/home": "Home",
+  "/learning": "Learning",
+  "/gaming": "Gaming",
+  "/memes": "Memes",
+};
+
+export function SiteHeader() {
+  const location = useLocation();
+  const title = routeTitles[location.pathname];
+
+  return (
+    <header className="flex justify-between h-16 shrink-0 items-center gap-2 px-4">
+      <div className="flex flex-row">
+        <SidebarTrigger className="-ml-1" />
+        <Separator
+          orientation="vertical"
+          className="mx-2 data-[orientation=vertical]:h-4"
+        />
+        <h1 className="text-base font-medium">{title}</h1>
+      </div>
+      <ModeToggle />
+    </header>
+  );
+}
