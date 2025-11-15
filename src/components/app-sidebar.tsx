cfc7024bc63640dc68c4c233ba8a7ae6dd6fd912
Schwifter <d.goglidze@sangu.edu.ge>
2025-11-15 17:19:49 +0400

    Add contacts

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,5 +1,13 @@
 import { Link, useLocation } from "react-router-dom";
-import { Home, GraduationCap, Gamepad2, Laugh } from "lucide-react";
+import {
+  Home,
+  GraduationCap,
+  Gamepad2,
+  Laugh,
+  Twitter,
+  Linkedin,
+  Github,
+} from "lucide-react";
 
 import {
   Sidebar,
@@ -34,6 +42,27 @@ const items = [
   },
 ];
 
+const itemsSecondary = [
+  {
+    id: 1,
+    title: "schwifterpickle",
+    url: "https://x.com/schwifterpickle",
+    icon: Twitter,
+  },
+  {
+    id: 2,
+    title: "d-goglidze",
+    url: "https://www.linkedin.com/in/d-goglidze/",
+    icon: Linkedin,
+  },
+  {
+    id: 3,
+    title: "datogoglidze",
+    url: "https://github.com/datogoglidze",
+    icon: Github,
+  },
+];
+
 export function AppSidebar() {
   const location = useLocation();
 
@@ -56,8 +85,25 @@ export function AppSidebar() {
             </SidebarMenuItem>
           ))}
         </SidebarMenu>
+        <SidebarMenu className="mt-auto">
+          {itemsSecondary.map((item) => (
+            <SidebarMenuItem key={item.id}>
+              <SidebarMenuButton asChild>
+                <a
+                  href={item.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:underline"
+                >
+                  <item.icon />
+                  {item.title}
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
       </SidebarContent>
-      <SidebarFooter>Side-Footer</SidebarFooter>
+      <SidebarFooter></SidebarFooter>
     </Sidebar>
   );
 }
