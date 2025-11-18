13d45b4e7baa157f4cdf86ec3565345a636ee4ee
David Goglidze <70262653+datogoglidze@users.noreply.github.com>
2025-11-18 14:01:11 +0400

    Refactor contacts data (#44)

---
+++ src/data/contact.ts
@@ -0,0 +1,38 @@
+import { Github, Linkedin, Mail, Twitter } from "lucide-react";
+import * as React from "react";
+
+export interface Contact {
+  id: number;
+  title: string;
+  url: string;
+  icon: React.ElementType;
+}
+
+export default function getContacts(): Contact[] {
+  return [
+    {
+      id: 1,
+      title: "schwifterpickle",
+      url: "https://x.com/schwifterpickle",
+      icon: Twitter,
+    },
+    {
+      id: 2,
+      title: "d-goglidze",
+      url: "https://www.linkedin.com/in/d-goglidze/",
+      icon: Linkedin,
+    },
+    {
+      id: 3,
+      title: "datogoglidze",
+      url: "https://github.com/datogoglidze",
+      icon: Github,
+    },
+    {
+      id: 4,
+      title: "d.goglidze22@gmail.com",
+      url: "mailto:d.goglidze22@gmail.com",
+      icon: Mail,
+    },
+  ];
+}
