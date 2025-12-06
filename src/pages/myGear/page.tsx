34006d86436c829ae545a84eb936fb6669b7078c
David Goglidze <d.goglidze@sangu.edu.ge>
2025-12-06 18:22:30 +0400

    Add Software list

--- src/pages/myGear/page.tsx
+++ src/pages/myGear/page.tsx
@@ -2,23 +2,39 @@ import { useDocumentTitle } from "@/hooks/useDocumentTitle";
 import hardwareColumns from "@/pages/myGear/hardwareColumns.tsx";
 import { DataTable } from "@/components/data-table.tsx";
 import { HARDWARE } from "@/data/hardware.ts";
+import { SOFTWARE } from "@/data/software.ts";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { TypographyList } from "@/components/typography-list.tsx";
 
 export default function MyGear() {
   useDocumentTitle("My Gear | Schwifter");
 
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-      <div className="bg-muted/50 rounded-xl p-6 pt-4">
-        <div className="flex flex-col xl:flex-row gap-10">
-          <div className="bg-muted/50 border border-transparent hover:border rounded-lg card-bounce flex flex-col p-6 pt-4 w-full">
-            <h1 className="text-2xl font-bold mb-4">Hardware</h1>
-            <DataTable columns={hardwareColumns} data={HARDWARE} />
-          </div>
+      <div className="bg-muted/50 rounded-xl md:min-h-min p-6 pt-4">
+        <h1 className="text-2xl font-bold mb-4">Hardware</h1>
+        <DataTable columns={hardwareColumns} data={HARDWARE} />
+      </div>
 
-          <div className="bg-muted/50 border border-transparent hover:border rounded-lg card-bounce flex flex-col  p-6 pt-4 w-full">
-            <h1 className="text-2xl font-bold mb-4">Software</h1>
-          </div>
-        </div>
+      <div className="bg-muted/50 rounded-xl md:min-h-min p-6 pt-4">
+        <h1 className="text-2xl font-bold mb-4">Software</h1>
+        <Accordion type="multiple" className="w-full border rounded-lg">
+          {SOFTWARE.map((software) => (
+            <AccordionItem value={String(software.id)}>
+              <AccordionTrigger className="px-2">
+                {software.category}
+              </AccordionTrigger>
+              <AccordionContent className="px-2">
+                {TypographyList({ items: software.names })}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
       </div>
     </div>
   );
