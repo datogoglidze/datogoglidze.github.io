e1934764d6a9c82904cdc579242b325fb8a9b3d4
Schwifter <d.goglidze@sangu.edu.ge>
2026-01-20 16:32:41 +0400

    Add sentry tracing

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,12 @@ import * as Sentry from "@sentry/react";
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
   environment: import.meta.env.VITE_SENTRY_ENVIRONMENT,
+  integrations: [Sentry.browserTracingIntegration()],
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
   sendDefaultPii: true,
+  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/datogoglidze\.github\.io/],
 });
 
 const container = document.getElementById("root")!;
