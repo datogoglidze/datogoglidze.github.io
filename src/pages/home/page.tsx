fe701d6f7a5d5f7c8eec39bd63771c40c5538bb6
Schwifter <d.goglidze@sangu.edu.ge>
2026-02-09 00:52:21 +0400

    Sort project chronologicaly

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -22,7 +22,7 @@ export default function HomePage() {
         <div className="grid auto-rows-min gap-4 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5">
           {PROJECTS.map((project) => (
             <ProjectCard key={project.id} project={project} />
-          ))}
+          )).reverse()}
         </div>
       </div>
     </div>
